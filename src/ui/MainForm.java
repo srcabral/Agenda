8fa72c9a32deab08d08cb6fcf26f8cab9b1497fd
Sergio Cabral <scastrocabral@gmail.com>
2021-09-18 16:21:00 -0300

    Criação da estrutura da agenda

---
+++ src/ui/MainForm.java
@@ -0,0 +1,10 @@
+package ui;
+
+import javax.swing.*;
+
+public class MainForm extends JFrame {
+    private JPanel rootPanel;
+    private JButton buttonNewContact;
+    private JButton buttonRemove;
+    private JTable tableContacts;
+}
